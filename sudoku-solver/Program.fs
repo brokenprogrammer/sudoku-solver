785262b3191bca91b021a678e7b2f8fee0774680
Oskar Mendel <brokenprogrammer@gmail.com>
2018-03-06 11:42:02 +0100

    Added digits generator function and isBlank curried function.

--- sudoku-solver/Program.fs
+++ sudoku-solver/Program.fs
@@ -2,11 +2,16 @@ module SudokuSolver
 
 open System
 
-type Box = Char
-type Matrix = Box list list
+type Digit = Char
+type Row = Digit list
+type Matrix = Row list
 
 let someSudoku : Matrix = 
     [['1'; '2';]; ['2'; '3';]]
+
+let digits : Box list = ['1'..'9']
+
+let isBlank = (=) '0'
 
 [<EntryPoint>]
 let main argv =
