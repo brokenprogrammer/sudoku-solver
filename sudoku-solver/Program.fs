8444f43af3b66fe02af9b7ca51634d87aafbc19c
Oskar Mendel <brokenprogrammer@gmail.com>
2018-03-07 05:11:51 +0100

    Implemented helper functions for expanding, checking duplicates..

--- sudoku-solver/Program.fs
+++ sudoku-solver/Program.fs
@@ -2,16 +2,47 @@ module SudokuSolver
 
 open System
 
+type Row<'T> = 'T list
+type Matrix<'T> = Row<'T> list
+
 type Digit = Char
-type Row = Digit list
-type Matrix = Row list
+type Choices = Digit list
+type Grid = Matrix<Digit>
 
-let someSudoku : Matrix = 
-    [['1'; '2';]; ['2'; '3';]]
+let undefined<'T> : 'T = failwith "Not implemented yet"
 
-let digits : Box list = ['1'..'9']
+let rows = (fun x -> x)
+//let cols = List.foldBack 
+
+let someSudoku : Grid = 
+    [['1'; '2';]; ['2'; '3'; '4';]]
+
+let digits : Digit list = ['1'..'9']
 
 let isBlank = (=) '0'
+
+let choices (g : Grid) : Matrix<Choices> = 
+    let choice d =
+        match d with
+            | ('0') -> digits
+            | _ -> d :: []
+    List.map (List.map choice) g
+
+let expand (m : Matrix<Choices>) : Grid list =
+    let op xs yss = [for x in xs do for ys in yss do yield x::ys]
+    let cp xss =
+        List.foldBack op xss [[]]
+    (List.map cp) m |> cp
+
+let rec noDuplicates l : bool =
+    match l with
+        | [] -> true
+        | x::xs -> (not (List.contains x xs)) && noDuplicates xs
+
+let valid (g : Grid) : bool =
+    List.forall noDuplicates (rows g)
+    //List.forall noDuplicates (cols g) &&
+    //List.forall noDuplicates (boxs g)
 
 [<EntryPoint>]
 let main argv =
